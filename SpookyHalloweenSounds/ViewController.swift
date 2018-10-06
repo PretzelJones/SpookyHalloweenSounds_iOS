a8080b8a1e859043ef07303bd8f258c900a2117d
Sean Patterson <sean@seans-macbook-pro.local>
2018-10-06 14:59:38 -0400

    Commit after MacOS reintstall

--- SpookyHalloweenSounds/ViewController.swift
+++ SpookyHalloweenSounds/ViewController.swift
@@ -55,8 +55,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //This is a test of github
         
         do {
             
